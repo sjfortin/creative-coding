14dc77e6b30776ae33e920b7c4aa128a5f99d46b
sjfortin <sam.j.fortin@gmail.com>
2023-05-23 15:11:26 -0500

    add basic shapes

--- sketch.js
+++ sketch.js
@@ -6,8 +6,17 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#7FB1FF';
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 100, 0, Math.PI * 2, false);
+    context.fillStyle = '#A1E9FF';
+    context.fill();
+
+    context.lineWidth = 30;
+    context.strokeStyle = '#BFC0FF';
+    context.stroke();
   };
 };
 
