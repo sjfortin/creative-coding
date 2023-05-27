7e3641324fed2e6a7a29cb6404ea1ddf79a4ba0c
sjfortin <sam.j.fortin@gmail.com>
2023-05-27 10:17:07 -0500

    radius and organic randomness

---
+++ colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+  blue: "#7FB1FF",
+  lightBlue: "#A1E9FF",
+  purple: "#BFC0FF",
+  darkPurple: "#7869F1",
+  pink: "#FFB6E4",
+  orange: "#FEC896",
+  yellow: "#FFE87F",
+  green: "#83FFC1",
+};
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
+const colors = require("./colors");
 
 const settings = {
   dimensions: [2048, 2048],
@@ -9,14 +10,17 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 200;
+    const count = 20;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
 
-        points.push([u, v]);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v],
+        });
       }
     }
 
@@ -30,15 +34,24 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const {
+        position: [u, v],
+        radius,
+      } = data;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 4, 0, Math.PI * 2, false);
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       context.strokeStyle = "black";
       context.lineWidth = 1;
-      context.stroke();
+      const keys = Object.keys(colors);
+      const randomKey = keys[Math.floor(Math.random() * keys.length)];
+      const randomColor = colors[randomKey];
+      context.fillStyle = randomColor;
+      context.fill();
     });
   };
 };
